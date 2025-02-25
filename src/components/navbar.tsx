f9272a5e80bcdb1a8ccadd3b5279ec81d9b9f26b
æLtorio <aeltorio@universidadatlantico.org>
2025-02-25 16:07:26 +0100

    feat: refactor Navbar component to use navItems from siteConfig and simplify link rendering

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -83,22 +83,19 @@ export const Navbar = () => {
 
       <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
+        {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
               <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                )}
+                color="foreground"
+                href={item.href}
               >
                 {item.label}
               </Link>
-            </NavbarMenuItem>
+            </NavbarItem>
           ))}
         </div>
       </NavbarMenu>
