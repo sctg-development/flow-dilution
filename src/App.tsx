dd83a369a2652b64ef22735d445423e0e8f04fc0
æLtorio <aeltorio@universidadatlantico.org>
2025-03-02 12:38:26 +0100

    feat: implement lazy loading for DilutionPage and CalibrationGasPage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { CalibrationGasPage } from "@/pages/calibrationgas";
-import { DilutionPage } from "@/pages/dilution";
+// Importez correctement les composants avec une déstructuration pour gérer les exports par défaut
+const DilutionPage = lazy(() =>
+  import("@/pages/dilution").then((module) => ({
+    default: module.DilutionPage,
+  })),
+);
+
+const CalibrationGasPage = lazy(() =>
+  import("@/pages/calibrationgas").then((module) => ({
+    default: module.CalibrationGasPage,
+  })),
+);
 
 export function App() {
   return (
     <Routes>
-      <Route element={<DilutionPage />} path="/" />
-      <Route element={<CalibrationGasPage />} path="/calibrationgas" />
+      <Route
+        element={
+          <Suspense fallback={<div>Page is Loading...</div>}>
+            <DilutionPage />
+          </Suspense>
+        }
+        path="/"
+      />
+      <Route
+        element={
+          <Suspense fallback={<div>Page is Loading...</div>}>
+            <CalibrationGasPage />
+          </Suspense>
+        }
+        path="/calibrationgas"
+      />
     </Routes>
   );
 }
