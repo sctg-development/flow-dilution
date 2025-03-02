c1b10ef3d0c9f9b0472749e4e5972b5d0bbd1a60
æLtorio <aeltorio@universidadatlantico.org>
2025-03-02 14:32:18 +0100

    feat: customize chunk naming and manual chunking strategy in Vite configuration for spltting the code

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,42 @@ export default defineConfig({
     exclude: ["@sctg/aga8-js"],
   },
   build: {
-    rollupOptions: {},
+    rollupOptions: {
+      output: {
+        // Personnaliser le nommage des chunks
+        entryFileNames: "js/flow-dilution-[hash].js",
+        chunkFileNames: "js/flow-dilution-[hash].js",
+        assetFileNames: "assets/flow-dilution-[hash].[ext]",
+        // Stratégie de regroupement des chunks
+        manualChunks: {
+          react: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "@react-aria/visually-hidden",
+          ],
+          sctg: ["@sctg/aga8-js", "@sctg/scientific-notation"],
+          heroui: [
+            "@heroui/button",
+            "@heroui/card",
+            "@heroui/dropdown",
+            "@heroui/input",
+            "@heroui/link",
+            "@heroui/navbar",
+            "@heroui/select",
+            "@heroui/skeleton",
+            "@heroui/slider",
+            "@heroui/switch",
+            "@heroui/system",
+            "@heroui/table",
+            "@heroui/tabs",
+            "@heroui/theme",
+            "@heroui/use-clipboard",
+          ],
+          tailwindcss: ["tailwind-variants", "tailwindcss"],
+          utilities: ["framer-motion"],
+        },
+      },
+    },
   },
 });
